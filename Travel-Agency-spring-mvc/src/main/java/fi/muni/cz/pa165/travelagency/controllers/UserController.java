3249db7e18f222064e4dd811893f93e2cb537530
Sevitas <442365@mail.muni.cz>
2017-12-19 21:56:44 +0100

    fix

--- Travel-Agency-spring-mvc/src/main/java/fi/muni/cz/pa165/travelagency/controllers/UserController.java
+++ Travel-Agency-spring-mvc/src/main/java/fi/muni/cz/pa165/travelagency/controllers/UserController.java
@@ -69,7 +69,7 @@ public class UserController {
             LOGGER.error("GET request: user/view");
             redirectAttributes.addFlashAttribute("alert_danger",
                     "You do not have administrator permission for editing other user.");
-            return "redirect:/user/view/" + id;
+            return "redirect:/user/view/" + authUser.getId();
         }
 
 
@@ -170,7 +170,7 @@ public class UserController {
     }
 
     /**
-     * Gets list of users
+     * Gets list of users, only admin can view all users
      * @param model model
      * @param req request
      * @param redirectAttributes redirect attributes
@@ -181,7 +181,7 @@ public class UserController {
                        HttpServletRequest req,
                        RedirectAttributes redirectAttributes) {
         if (!isAuthenticated(req, redirectAttributes, true)) {
-
+            redirectAttributes.addFlashAttribute("alert_danger", "Admin role required");
             return AUTH_PAGE_URL;
         }
 
